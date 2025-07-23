78a351bc41bc4800f3e077cba3831214f63f4a62
rethi <rethi@MyPC26>
2025-07-23 12:23:02 +0530

    Team project

--- ShoppingMall/src/ShoppingMall/entity/CusDetails.java
+++ ShoppingMall/src/ShoppingMall/entity/CusDetails.java
@@ -5,6 +5,7 @@ public class CusDetails {
 	public static void main(String[] args) {
 		System.out.println("Welcome to our Shopping Mall");
 		System.out.println("hello ");
+		System.out.println("harshitha");
 
 	}
 
